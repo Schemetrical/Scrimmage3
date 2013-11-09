4d7948bc675c82cd73d1a7590851e59878740281
Jake <jake0oo0andminecraft@gmail.com>
2013-11-09 14:20:59 -0600

    Add licensing, etc

--- src/main/java/io/github/ocnscrim/scrimmage/modules/TimeLockModule.java
+++ src/main/java/io/github/ocnscrim/scrimmage/modules/TimeLockModule.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 Jake0oo0.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package io.github.ocnscrim.scrimmage.modules;
 
 
@@ -7,9 +30,20 @@ import io.github.ocnscrim.scrimmage.utils.StringUtils;
 import io.github.ocnscrim.scrimmage.utils.XMLUtils;
 import org.w3c.dom.Node;
 
+/**
+ * Class for controlling day/night timelock
+ *
+ * @author Jake0oo0
+ */
 public class TimeLockModule extends Module {
     boolean enabled;
 
+    /**
+     * Default constructor using superclass Module constructor
+     *
+     * @param mat
+     * @param map
+     */
     public TimeLockModule(Match mat, Map map) {
         super(mat, map);
         Node n = XMLUtils.getFirstNodeByName(x.getDoc(), "timelock");
@@ -18,6 +52,9 @@ public class TimeLockModule extends Module {
         }
     }
 
+    /**
+     * @return Whether or not the timelock is enabled
+     */
     public boolean isEnabled() {
         return enabled;
     }
