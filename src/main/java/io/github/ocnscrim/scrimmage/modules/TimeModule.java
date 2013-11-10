652574d2ce66346804c34af1b75f7ad7446d28c2
Jake <jake0oo0andminecraft@gmail.com>
2013-11-09 20:17:33 -0600

    Add time module

--- src/main/java/io/github/ocnscrim/scrimmage/modules/TimeModule.java
+++ src/main/java/io/github/ocnscrim/scrimmage/modules/TimeModule.java
@@ -25,20 +25,15 @@ package io.github.ocnscrim.scrimmage.modules;
 
 import io.github.ocnscrim.scrimmage.map.Map;
 import io.github.ocnscrim.scrimmage.match.Match;
-import io.github.ocnscrim.scrimmage.utils.StringUtils;
+import io.github.ocnscrim.scrimmage.utils.TimeUtils;
 import io.github.ocnscrim.scrimmage.utils.XMLUtils;
-import org.bukkit.entity.EntityType;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
- * Class for modifying the projectile that a bow shoots
- *
- * @author Jake0oo0
+ * Module for managing match time
  */
-public class ModifiedBowModule extends Module {
-    EntityType type;
+public class TimeModule extends Module {
+    int l;
 
     /**
      * Default constructor using superclass Module constructor
@@ -46,30 +41,15 @@ public class ModifiedBowModule extends Module {
      * @param mat
      * @param map
      */
-    public ModifiedBowModule(Match mat, Map map) {
+    public TimeModule(Match mat, Map map) {
         super(mat, map);
-        Node n = XMLUtils.getFirstNodeByName(x.getDoc(), "modifybowprojectile");
+        Node n = XMLUtils.getFirstNodeByName(x.getDoc(), "time");
         if (n != null) {
-            if (n.getNodeType() == Node.ELEMENT_NODE) {
-                NodeList ns = n.getChildNodes();
-                for (int c = 0; c < ns.getLength(); c++) {
-                    Node nc = ns.item(c);
-                    if (nc.getNodeType() == Node.ELEMENT_NODE) {
-                        Element e = (Element) nc;
-                        switch(e.getTagName()) {
-                            case "projectile":
-                                type = StringUtils.getEntityTypeFromString(e.getTextContent());
-                                break;
-                            case "velocity":
-                                 // Implement velocity support
-                        }
-                    }
-                }
-            }
+            l = TimeUtils.parseTimeStringIntoSecs(n.getTextContent());
         }
     }
 
-    public EntityType getType() {
-        return type;
+    public int getMatchLength() {
+        return l;
     }
 }
